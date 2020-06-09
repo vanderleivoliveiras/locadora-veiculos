6a5be402d52419ff73d76ffeacb8f5c7f69fec05
Vanderlei Santos - DTI <dti232@localiza.com>
2020-06-08 22:29:52 -0300

    Implementação test mocha para crud

--- Backend/server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-const mongoose = require('./database/mongoose');
-
-mongoose.connect();
--- Backend/test/server.test.js
+++ Backend/test/server.test.js
@@ -1,67 +1,64 @@
-process.env.ENV = 'test'
-
 const request = require('supertest');
-const mongoose = require('../database/mongoose');
-const app = require('../app');
+const app = require('../app').app;
 const Veiculo = require('../database/models/veiculo');
 var assert = require('assert');
 
-// it('Teste de chamada get/veiculo sem erro', (done) => {
-//     request(app)
-//         .get('/veiculo')
-//         .expect(200)
-//         .end(done);
-// });
+it('Teste de chamada get/veiculo sem erro', (done) => {
+    request(app)
+    .get('/veiculo')
+    .expect(200)
+    .end(done);
+});
 
-describe('POST /veiculo', function () {
-    before((done)=>{
-        mongoose.connect()
-        .then(() => done())
-        .catch((erro) => done(erro))
-    })
+describe('Veiculo', function() {
+    describe('#save()', function() {
+      it('Deve salvar sem erro', function(done) {
+        var veiculo = new Veiculo();
+        veiculo.placa = "ABC-1234";
+        veiculo.chassi = "abc1234b333ccc1c2";
+        veiculo.modelo = "Uno";
+        veiculo.renavam = "1223bb48883";
+        veiculo.marca = "Fiat";
+        veiculo.ano = "93/94";
 
-    before((after)=>{
-        mongoose.connect()
-        .then(() => done())
-        .catch((erro) => done(erro))
-    })
-    
+        veiculo.save(done);
+      });
+    });
+  });
 
-    it('Criação de veiculo deve funcionar', (done) => {
-        request(app).post('/veiculo')
-            .send({
-                placa: "ABC-1234",
-                chassi: "abc1234b333ccc1c2",
-                modelo: "Uno",
-                renavam: "1223bb48883",
-                marca: "Fiat",
-                ano: "93/94"
-            })
-            .then(res =>{
-                const body = res.body;
-                expect(body).to.contains.property('_id');
-                expect(body).to.contains.property('placa');
-                expect(body).to.contains.property('modelo');
-                expect(body).to.contains.property('renavam');
-                done();
-            })
-            .catch(erro => {done(erro)})
-    })
-})
+  describe('Veiculo', function() {
+    describe('#remove()', function() {
+      it('Deve deletar sem erro', function(done) {
+        var veiculo = new Veiculo();
+        //Buscar id e deletar dinamicamente
+        veiculo._id = "5edee1429b28363328f9e589";
 
-// describe('Veiculo', function() {
-//     describe('#save()', function() {
-//       it('Deve salvar sem erro', function(done) {
-//         var veiculo = new Veiculo();
-//         veiculo.placa = "ABC-1234";
-//         veiculo.chassi = "abc1234b333ccc1c2";
-//         veiculo.modelo = "Uno";
-//         veiculo.renavam = "1223bb48883";
-//         veiculo.marca = "Fiat";
-//         veiculo.ano = "93/94";
+        veiculo.remove(done);
 
-//         veiculo.save(done);
-//       });
-//     });
-//   });
+        // it('Teste de chamada get/veiculo sem erro', (done) => {
+        //     request(app)
+        //     .get('/veiculo')
+        //     .expect(200)
+        //     .end(done);
+        // });
+      });
+    });
+  });
 
+
+  describe('Veiculo', function() {
+    describe('#update()', function() {
+      it('Deve modificar sem erro', function(done) {
+        var veiculo = new Veiculo();
+        veiculo._id = "5edee4f7e27ac50844e4975e";
+        veiculo.placa = "3";
+        veiculo.chassi = "abc1234b333ccc1c2";
+        veiculo.modelo = "Uno";
+        veiculo.renavam = "1223bb48883";
+        veiculo.marca = "Fiat";
+        veiculo.ano = "93/94";
+
+        veiculo.updateOne(() => {},(done));
+      });
+    });
+  });
